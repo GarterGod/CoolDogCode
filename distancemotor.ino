eb359002871a413e2f5d15fa4bd68bf41689f5fa
GarterGod <149113964+GarterGod@users.noreply.github.com>
2024-04-09 18:09:49 -0400

    a motor activated from a distance

---
+++ distancemotor.ino
@@ -0,0 +1,51 @@
+
+#define  MAX_RANG      (520)//the max measurement vaule of the module is 520cm(a little bit longer than  effective max range)
+#define  ADC_SOLUTION      (1023.0)//ADC accuracy of Arduino UNO is 10bit
+#define  Inch_Convert  (2.54)
+#define Foot_Convert (30.48)
+int sensityPin = A0;    // select the input pin 
+const int motor = 9;    // the number of the LED pin
+
+
+
+void setup() {
+  // Serial init
+  Serial.begin(9600);
+   pinMode(motor, OUTPUT);
+
+}
+float dist_t, sensity_t, dist_f ;
+void loop() {
+  // read the value from the sensor:
+ sensity_t = analogRead(sensityPin);
+  // turn the ledPin on
+
+ dist_f = sensity_t * MAX_RANG  / ADC_SOLUTION;//
+ dist_f= dist_f/ Foot_Convert;// 
+ Serial.print(dist_f,1);
+ Serial.println("ft");
+
+
+
+ dist_t = sensity_t * MAX_RANG  / ADC_SOLUTION;//
+ dist_t= dist_t/ Inch_Convert;// 
+ Serial.print(dist_t,1);
+ Serial.println("in");
+
+
+ // check if distance is greater than a threshold
+  if (dist_t > 12.0) {    // for example, if distance in feet is greater than 5.0
+
+        digitalWrite(motor, HIGH);
+
+
+
+
+  } else {
+    Serial.print("Distance less than or equal to 12 inches");
+    digitalWrite(motor,LOW);
+
+     // print distance with 2 decimal places
+
+  }
+}
